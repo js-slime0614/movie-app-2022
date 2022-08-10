da1e386df6d21e94e67c6acc6a3fb5eecf4635d6
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2022-08-10 09:01:54 +0900

    style

--- src/Movie.js
+++ src/Movie.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres }) {
-    return <div class="movie">
+    return <div className="movie">
         <img src={poster} alt={title} title={title}/>
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
+        <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
             <ul className='genres'>{genres.map((genre, index) => <li key={index} className='genres__genre'>{genre}</li>)}</ul>
-            <p class="movie_summary">{summary}</p>
+            <p className="movie_summary">{summary}</p>
         </div>
     </div>;
 }
